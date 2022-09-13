d936da53f1aff1b1d28ef597ab763e187ba6aaac
alvin.z <alvin.z.ca@gmail.com>
2022-09-13 16:05:00 -0400

    Update sign.py

--- graphql_st/schemas/sign.py
+++ graphql_st/schemas/sign.py
@@ -165,22 +165,28 @@ class SendSMS(graphene.Mutation):
         apiKey = ICP.get_param('st_sms_api_key', False)
         number = ICP.get_param('st_sms_number', False)
 
-        # https://api.genvoice.net/docs/#api-SMS-SendSMSwithoutFrom
-        url = apiUrl + '/api/sms/send/'
-        if (number):
-            url = url + "/" + number
-        url = url + "/" + cellphone
+        try
+            # https://api.genvoice.net/docs/#api-SMS-SendSMSwithoutFrom
+            url = apiUrl + '/api/sms/send/'
+            if (number):
+                url = url + "/" + number
+            url = url + "/" + cellphone
+                
+            data = {'text': message, 'sign': 'ST'}
             
-        data = {'text': message, 'sign': 'ST'}
-        
-        headers = {'Content-type': 'application/json', 'x-app-key': apiKey}
-        
-        r = requests.post(url, data=json.dumps(data), headers=headers)
+            headers = {'Content-type': 'application/json', 'x-app-key': apiKey}
+            
+            r = requests.post(url, data=json.dumps(data), headers=headers)
 
-        if r.status_code == '200':
-            return True
-        else:
-            raise GraphQLError(_('SMS failed.'))
+            if r.status_code == '200':
+                return True
+            else:
+                raise GraphQLError(_('SMS failed.'))
+        except requests.exceptions.Timeout:
+            raise GraphQLError(_('Failed: SMS Gateway Timeout'))
+        except requests.exceptions.RequestException as e:
+            raise GraphQLError(_(string.format('Failed: %s', str(e)))
+            
 
 class SignMutation(graphene.ObjectType):
     login = Login.Field(description='Authenticate user with email and password and retrieves token.')
