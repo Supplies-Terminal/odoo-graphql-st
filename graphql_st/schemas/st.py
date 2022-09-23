576c09d26f7e2121795ef9ae05ff2d7fc6e9b24f
alvin.z <alvin.z.ca@gmail.com>
2022-09-23 14:14:59 -0600

    增加aws textact功能

--- graphql_st/schemas/st.py
+++ graphql_st/schemas/st.py
@@ -2,6 +2,9 @@
 # Copyright 2022 ODOOGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import json
+import boto3
+import base64
 import graphene
 from graphql import GraphQLError
 from odoo import _
@@ -132,7 +135,64 @@ class UpdatePreference(graphene.Mutation):
 
         preference = env['st.preference'].search([('member_id', '=', partner.id)], limit=1)
         return preference
+
+### purchase card OCR ###
+class ocrPurchasecardParams(graphene.InputObjectType):
+    supplier_id = graphene.Int(required=True)
+    image_base64 = graphene.String(required=True)
+
+class OcrPurchasecard(graphene.Mutation):
+    class Arguments:
+        params = ocrPurchasecardParams()
+
+    Output = StPurchasecard
+
+    @staticmethod
+    def mutate(self, info, params):
+        uid = request.session.uid
+        env = info.context["env"]
+        user = env['res.users'].sudo().browse(uid)
+        if not user:
+            raise GraphQLError(_('User does not exist.'))
+
+        partner = user.partner_id
+        if not partner:
+            raise GraphQLError(_('Partner does not exist.'))
+
+        purchasecard = env['st.purchasecard'].search([('member_id', '=', partner.id), ('supplier_id', '=', params.supplier_id)], limit=1)
+
+        if not purchasecard:
+            raise GraphQLError(_('Purchase Card does not exist.'))
+
+        imageBase64 = params.image_base64
+        
+        # Amazon Textract client
+        textractClient = boto3.client('textract',
+                                aws_access_key_id='AKIASNQ3NZMG2Y3UEBKL',
+                                aws_secret_access_key='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',
+                                region_name='ca-central-1')
+        # Call Amazon Textract
+        analyzeDocumentResponse = textractClient.analyze_document(
+            Document={
+                'Bytes': content
+            },
+            FeatureTypes=[
+                'TABLES'
+            ]
+        )
+        print(analyzeDocumentResponse)
+
+        blocks = analyzeDocumentResponse['Blocks']
+        print(analyzeDocumentResponse)
+
+        # tables = [] # 所有tables
+        # allWords = [] # 所有类别
+        # allCells = []
+        
+        purchasecard.data = json.encode(blocks)
+        return purchasecard
     
 class StMutation(graphene.ObjectType):
-    update_purchasecard = UpdatePurchasecard.Field(description='Set purchase card for a specific supplier')
     update_Preference = UpdatePreference.Field(description='Update user preferrences on App')
+    update_purchasecard = UpdatePurchasecard.Field(description='Set purchase card for a specific supplier')
+    ocr_purchasecard = OcrPurchasecard.Field(description='Textract purchase card image')
