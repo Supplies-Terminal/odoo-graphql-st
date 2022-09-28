f19ec0c37ff1a3382f69be139e3b879ab6d242c3
alvin.z <alvin.z.ca@gmail.com>
2022-09-28 16:59:45 -0600

    Update purchasecard.py

--- graphql_st/schemas/purchasecard.py
+++ graphql_st/schemas/purchasecard.py
@@ -117,7 +117,7 @@ class OcrPurchasecard(graphene.Mutation):
         bucket_name = 'purchasecard'
         file_name_with_extention = purchasecard['uuid'] + '.jpg'
         file = io.BytesIO(base64.b64decode(image_base64))
-        obj = s3.upload_fileobj(file, bucket_name,file_name_with_extention, ExtraArgs={'ACL': 'public-read'})
+        obj = s3.upload_fileobj(file, bucket_name,file_name_with_extention)
         # location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
         # object_url = "https://%s.s3-%s.amazonaws.com/%s" % (bucket_name, location, file_name_with_extention)
         
