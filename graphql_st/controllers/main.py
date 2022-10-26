95ac1a81f308a9a53ca138d74e567a77fa536001
alvin.z <alvin.z.ca@gmail.com>
2022-10-26 01:01:06 -0600

    Update main.py

--- graphql_st/controllers/main.py
+++ graphql_st/controllers/main.py
@@ -105,10 +105,8 @@ class GraphQLController(http.Controller, GraphQLControllerMixin):
         # resp.headers['Access-Control-Allow-Origin'] = 'https://webapp2.suppliesterminal.com'
         # resp.headers['Access-Control-Allow-Methods'] = 'GET, POST'
         resp.headers['Access-Control-Allow-Credentials'] = 'true'
-        resp.set_cookie(
-                samesite='None'
-            )
-        resp.set_cookie('SameSite', 'None')
+        resp.set_cookie('cookie2', 'value2', samesite='None', secure=True)
+        resp.headers.add('Set-Cookie','cookie3=value3; SameSite=None; Secure')
         return resp
 
     @http.route('/st/categories', type='http', auth='public', csrf=False, cors='*')
