acdafe1a20dd60b7d2a87f20793fe41f32f0690a
alvin.z <alvin.z.ca@gmail.com>
2022-10-25 23:37:39 -0600

    使用方法重写来扩充http基础函数以处理cors问题

--- graphql_st/controllers/main.py
+++ graphql_st/controllers/main.py
@@ -4,10 +4,10 @@
 
 import os
 import json
-from odoo import http
+from odoo import http,
 from odoo.addons.web.controllers.main import Binary
 from odoo.addons.graphql_base import GraphQLControllerMixin
-from odoo.http import request, Response
+from odoo.http import request, Response, HttpRequest
 from odoo.tools.safe_eval import safe_eval
 from urllib.parse import urlparse
 import logging
@@ -169,3 +169,56 @@ class GraphQLController(http.Controller, GraphQLControllerMixin):
             json.dumps(redirects),
             headers={'Content-Type': 'application/json'},
         )
+
+
+
+def dispatch(self):
+    if self._is_cors_preflight(request.endpoint):
+        _logger.info("------ ***** Dispatcher ***** -----")
+        headers = {
+            'Access-Control-Max-Age': 60 * 60 * 24,
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return Response(status=200, headers=headers)
+
+    if request.httprequest.method not in ('GET', 'HEAD', 'OPTIONS', 'TRACE') \
+            and request.endpoint.routing.get('csrf', True): # csrf checked by default
+        token = self.params.pop('csrf_token', None)
+        if not self.validate_csrf(token):
+            if token is not None:
+                _logger.warning("CSRF validation failed on path '%s'",
+                                request.httprequest.path)
+            else:
+                _logger.warning("""No CSRF validation token provided for path '%s'
+
+Odoo URLs are CSRF-protected by default (when accessed with unsafe
+HTTP methods). See
+https://www.odoo.com/documentation/15.0/developer/reference/addons/http.html#csrf for
+more details.
+
+* if this endpoint is accessed through Odoo via py-QWeb form, embed a CSRF
+token in the form, Tokens are available via `request.csrf_token()`
+can be provided through a hidden input and must be POST-ed named
+`csrf_token` e.g. in your form add:
+
+    <input type="hidden" name="csrf_token" t-att-value="request.csrf_token()"/>
+
+* if the form is generated or posted in javascript, the token value is
+available as `csrf_token` on `web.core` and as the `csrf_token`
+value in the default js-qweb execution context
+
+* if the form is accessed by an external third party (e.g. REST API
+endpoint, payment gateway callback) you will need to disable CSRF
+protection (and implement your own protection if necessary) by
+passing the `csrf=False` parameter to the `route` decorator.
+                """, request.httprequest.path)
+
+            raise werkzeug.exceptions.BadRequest('Session expired (invalid CSRF token)')
+
+    r = self._call_function(**self.params)
+    if not r:
+        r = Response(status=204)  # no content
+    return r
+
+HttpRequest.dispatch = dispatch
